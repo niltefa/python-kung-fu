b96a713d1a3f1750a043282206ba8bd5dc111b8a
Nil Farré Terron <91869645+niltefa@users.noreply.github.com>
2022-05-02 14:34:34 +0200

    Solution_Challenge_4

--- 04.py
+++ 04.py
@@ -9,7 +9,12 @@ from __future__ import print_function
 """ How do you count the number of digits of an integer? """
 
 def ndigits(number):
-    pass # place magic here
+    str_num = str(number)
+    if str_num[0] == '-':
+        num = len(str(str_num))-1
+    else:
+        num = len(str(str_num))
+    return num
 
 if __name__ == "__main__":
 
@@ -17,4 +22,3 @@ if __name__ == "__main__":
     print(ndigits(-5))  # 1
     print(ndigits(27))  # 2
     print(ndigits(495)) # 3
-
